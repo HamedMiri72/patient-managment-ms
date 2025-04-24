1f9627a624883ee74378be9aaaac049dd1420f81
HamedMiri72 <hamedmiri72@gmail.com>
2025-04-25 00:31:38 +0100

    run billingmicroservice

--- services/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ services/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,8 +1,35 @@
 package com.pm.billingservice.grpc;
 
+import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import net.devh.boot.grpc.server.service.GrpcService;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
+
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver){
+
+        log.info("createBillingAccount request received{}", billingRequest.toString());
+
+        // Business logic
+
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
 }
